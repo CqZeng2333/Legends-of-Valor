5b250d17ed3ae8fe06d965c5b307d1da370b0ad4
Ziang Leng <zaleng@bu.edu>
2020-11-21 17:54:00 +0800

    add Legends of Valor

---
+++ src/LegdensOfValor.java
@@ -0,0 +1,22 @@
+
+public class LegdensOfValor extends RolePlayingGame {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int fight(Plain tile) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int action(String input) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
